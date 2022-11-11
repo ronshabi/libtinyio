91658dc86cdbd7c9969c38b75c5b4e7ed993ccaa
ron <@>
2022-11-11 21:30:56 +0200

    Added: strlen function

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 clear
 executable="libtinyio"
 executable_suffix="out"
-task_compile="build"
+task_compile=""
 task_strace="strace"
 task=${1}
 
@@ -14,5 +14,5 @@ elif [ -a "${executable}.${executable_suffix}" ] && [ $task == $task_strace ] ; 
     # strace if file exists
     ${task_strace} ./${executable}.${executable_suffix}
 else
-    echo "${task_strace}: file doesn't exist."
+    echo "Target doesn't exist."
 fi
--- libtinyio.asm
+++ libtinyio.asm
@@ -9,7 +9,27 @@ nlTest: db 10
 section .text
 
 _start:
+mov rdi, strTest
+call string_length
+mov rdi, rax
 jmp exit
+
+string_length:
+    ; Returns null-terminated string length
+    ; arguments: rdi - string ptr
+    mov rax, rdi
+    xor rcx, rcx
+
+.check_null:    
+    cmp byte[rax+rcx], 0
+    jnz .increment
+    jmp .return_str_len
+.increment:
+    inc rcx
+    jmp .check_null
+.return_str_len:
+    mov rax, rcx
+    ret
 
 exit:
     ; Sends Exit (60) syscall
