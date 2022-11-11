bc7f326dbd23c61c76ecf803402ae246fc23d961
ron <@>
2022-11-12 00:14:41 +0200

    Added: print_char

--- libtinyio.asm
+++ libtinyio.asm
@@ -3,16 +3,24 @@
 global _start
 
 section .data
+; for testing purposes
 strTest: db "abcdefABCDEF123", 0
 nlTest: db 10
 
 section .text
 
+; ------ for testing ------
 _start:
 mov rdi, strTest
 call string_length
-mov rdi, rax
+
+;; print char
+mov rdi, 0x41 ; 'A'
+call print_char
+
+mov rdi, 1
 jmp exit
+; --------/_start----------
 
 string_length:
     ; Returns null-terminated string length
@@ -29,6 +37,19 @@ string_length:
     jmp .check_null
 .return_str_len:
     mov rax, rcx
+    ret
+
+print_char:
+    ; Prints a character to stdout
+    ; args: rdi - character code to print
+    push rdi     ; push character code to stack
+    xor rax, rax
+    mov rax, 1   ; sys_write
+    mov rsi, rsp ; buf
+    mov rdi, 1   ; stdout
+    mov rdx, 1   ; count
+    syscall
+    pop rdi      ; restore prev state
     ret
 
 exit:
