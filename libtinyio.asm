5e6f137cd9e5ebca065a97b51346d6a321a0e7eb
ron <@>
2022-11-13 01:44:55 +0200

    Added: print_string

--- libtinyio.asm
+++ libtinyio.asm
@@ -8,10 +8,11 @@ strTest: db "abcdefABCDEF123", 0
 
 ; constants
 ASCII_NEW_LINE  equ 10
-FILE_STDOUT     equ 0x1
+FD_STDOUT     equ 0x1
 
 ; syscalls
-SYS_WRITE   equ 0x1
+SYS_READ    equ 0x00
+SYS_WRITE   equ 0x01
 SYS_EXIT    equ 0x3c
 
 
@@ -20,12 +21,7 @@ section .text
 ; ------ for testing ------
 _start:
 mov rdi, strTest
-call string_length
-
-;; print char
-mov rdi, 0x41 ; 'A'
-call print_char
-call print_newline
+call print_string
 
 mov rdi, 1
 jmp exit
@@ -36,39 +32,62 @@ string_length:
     ; arguments: rdi - string ptr
     mov rax, rdi
     xor rcx, rcx
-
 .check_null:    
     cmp byte[rax+rcx], 0
     jnz .increment
-    jmp .return_str_len
+    mov rax, rcx
+    ret
 .increment:
     inc rcx
     jmp .check_null
-.return_str_len:
-    mov rax, rcx
-    ret
+
 
 print_char:
     ; Prints a character to stdout
     ; args: rdi - character code to print
-    push rdi     ; push character code to stack
+    push rdi                ; push character code to stack
     xor rax, rax
-    mov rax, SYS_WRITE      ; sys_write
+    mov rax, SYS_WRITE 
     mov rsi, rsp            ; buf
-    mov rdi, FILE_STDOUT    ; stdout
+    mov rdi, FD_STDOUT      
     mov rdx, 1              ; count
     syscall
     pop rdi                 ; restore prev state
     ret
 
+
 print_newline:
+    ; Prints a newline ('\n') character
+    ; args: none
     xor rdi, rdi
     mov rdi, ASCII_NEW_LINE
     call print_char
     ret
 
+
+print_string:
+    ; Prints a null-terminated string
+    ; args: rdi - memory location to a c-style string
+
+    xor r10, r10
+    mov r10, rdi
+    xor r8, r8
+.check_for_null:
+    cmp byte[r8+r10], 0
+    jnz .print
+    ret
+.print:
+    mov rax, SYS_WRITE
+    mov rdi, FD_STDOUT
+    lea rsi, [r8+r10]
+    mov rdx, 1 ; count
+    syscall
+    inc r8
+    jmp .check_for_null
+
+
 exit:
     ; Sends Exit (0x3c) syscall
-    ; arguments: rdi - exit code
+    ; args: rdi - exit code
     mov rax, SYS_EXIT
     syscall
